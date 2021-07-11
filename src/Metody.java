9520a3e53524d1d95f131567148fb52525bf703e
testuje bartosz <testuje.bartosz@gmail.com>
2021-07-11 19:17:30 +0200

    sample git comment

--- src/Metody.java
+++ src/Metody.java
@@ -4,11 +4,39 @@ public class Metody {
 
         System.out.println("Zaraz policzę wynik");
         int result = 0;
-        for ( int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             result = result + i;
             System.out.println(result);
+        }
+        System.out.println("Rezultat to: " + result);
+    }
+
+    public int pobierzWynik() {
+        System.out.println("Zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            result = result + i;
 
         }
         System.out.println("Rezultat to: " + result);
+        return result;
+    }
+
+    public void policzWynikParam(int number) {
+        System.out.println("Number ma wartosc: " + number);
+        System.out.println("Zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result + i;
+        }
+        System.out.println("Rezultat to: " + result);
+
+
+    }
+
+    public int add(int firstNumber, int secondNumber, String word) {
+        //System.out.println("Suma to: " + (firstNumber + secondNumber));
+        System.out.println(word);
+        return firstNumber+secondNumber;
     }
 }
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -3,6 +3,21 @@ public class MetodyTest {
     public static void main(String[] args) {
 
         Metody metody = new Metody();
+     /*   metody.policzWynik();
         metody.policzWynik();
+        metody.policzWynik();*/
+       /* int result = metody.pobierzWynik();
+        int result2 = result *2;
+        System.out.println("Rezultat po mnozeniu to: " + result2);*/
+        /*metody.policzWynikParam(2);
+        metody.policzWynikParam(100);
+        metody.policzWynikParam(23);*/
+
+        //metody.add(1,3);
+        int result = metody.add(2,3, "Hello");
+        System.out.println(metody.add(2,6, "Mis!"));
+        System.out.println(result);
     }
+
+
 }
